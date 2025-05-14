8f133d7377432028961eb779ca9d0d025dcf0b5b
Zellowgram <eustachio.antonio1999@gmail.com>
2025-05-14 14:47:55 +0200

    Aggiunti comandi al controller
    .

--- src/main/java/corso/java/controllers/AuthorController.java
+++ src/main/java/corso/java/controllers/AuthorController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import corso.java.dto.AuthorDto;
@@ -25,10 +28,24 @@ public class AuthorController {
 		return ResponseEntity.ok(authorService.getAllAuthors());
 	}
 	
-	@PostMapping
+	@PostMapping("/add")
 	public ResponseEntity<AuthorDto> addAuthors(@RequestBody AuthorDto a){
 		authorService.addAuthor(a);
 		return ResponseEntity.ok(a);
 	}
-
+	
+	@PostMapping("/delete")
+	public void deleteAuthor(@RequestParam int authorId) {
+		authorService.deleteAuthor(authorId);
+	}
+	
+	@GetMapping("/{username}")
+	public ResponseEntity<AuthorDto> getAuthorByUsername(@PathVariable String username){
+		return ResponseEntity.ok(authorService.getAuthorByUsername(username));
+	}
+	
+	@PutMapping("/update")
+	public void updateAuthor(@RequestBody AuthorDto author) {
+		authorService.updateAuthor(author);
+	}
 }
