52acaa5594c7c83a479adf8cb24ac1aba54ab54d
yenomack <giammi.petrone@virgilio.it>
2025-05-14 11:55:26 +0200

    push properties update

---
+++ src/main/java/corso/java/BlogAuthNeverHackApplication.java
@@ -0,0 +1,13 @@
+package corso.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAuthNeverHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAuthNeverHackApplication.class, args);
+	}
+
+}
