68a9184bcad133f1b0440b11a96ebc8ed6abcd0c
Zellowgram <eustachio.antonio1999@gmail.com>
2025-05-14 16:28:57 +0200

    Rimossi conflitti
    .

--- src/main/java/corso/java/repositories/AuthorRepository.java
+++ src/main/java/corso/java/repositories/AuthorRepository.java
@@ -5,16 +5,11 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import corso.java.dto.AuthorDto;
 import corso.java.entities.AuthorEntity;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<AuthorEntity, Integer>{
 	
-<<<<<<< Updated upstream
-	public AuthorEntity findByUsername(String username);
+	public Optional <AuthorEntity> findByUsername(String username);
 
-=======
-	public Optional <AuthorEntity > findByUsername(String username);
->>>>>>> Stashed changes
 }
